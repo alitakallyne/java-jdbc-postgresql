e66690a8089b975af3ffd32778e26676cab264d0
alita_kallyne <alytakallyne@gmail.com>
2024-10-21 13:09:39 -0300

    Salvando no banco

--- src/main/java/dao/UserPosDAO.java
+++ src/main/java/dao/UserPosDAO.java
@@ -1,8 +1,11 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.UserPosJava;
 
 public class UserPosDAO {
 
@@ -10,5 +13,28 @@ public class UserPosDAO {
 	
 	public UserPosDAO() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar(UserPosJava userPosJava){
+		
+		try {
+			
+		String sql = "insert into userposjava (id,nome,email) values (?,?,?);";
+		PreparedStatement insert = connection.prepareStatement(sql);
+		insert.setLong(1, userPosJava.getId());
+		insert.setString(2, userPosJava.getNome());
+		insert.setString(3, userPosJava.getEmail());
+		insert.execute(); 
+		connection.commit();//Salvar no banco
+		
+		}catch (Exception e) {
+			
+			try {
+				connection.rollback(); //Reverte a operação
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,11 +3,22 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.jupiter.api.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.UserPosJava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco(){
-	   SingleConnection.getConnection();
+	   //SingleConnection.getConnection();
+		
+		UserPosDAO userPosDAO = new UserPosDAO();
+		UserPosJava  userPosJava = new UserPosJava();
+		
+		userPosJava.setId(4L);
+		userPosJava.setNome("maria");
+		userPosJava.setEmail("maria@gamil.com");
+		
+		userPosDAO.salvar(userPosJava);
 	}
 }
